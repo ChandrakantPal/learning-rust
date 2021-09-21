9294297b8ee45e1e1013db336048a2bc02f1611f
ChandrakantPal <sanjay161097@gmail.com>
2021-09-22 00:08:23 +0530

    actvity 1 functions

---
+++ activities/src/bin/a1.rs
@@ -0,0 +1,24 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+// * Use a function to display your first name
+fn first_name() {
+    println!("Sanjay");
+}
+
+// * Use a function to display your last name
+fn last_name() {
+    println!("Pal");
+}
+
+fn main() {
+    first_name();
+    last_name();
+}
