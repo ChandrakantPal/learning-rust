bc1b2539a9c68371b6904c80436fdf821f2fde17
ChandrakantPal <sanjay161097@gmail.com>
2021-09-23 23:31:31 +0530

    mathmatical operators

---
+++ activities/src/bin/cwt-numeric-types.rs
@@ -0,0 +1,11 @@
+fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
+fn main() {
+    let sum = 2 + 2;
+    let value = sub(10, 5);
+    let division = 10 / 2;
+    let mult = 5 * 5;
+    let rem = 6 % 3;
+    let rem2 = 6 % 4;
+}
