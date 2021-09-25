6ed902cd62a8a79724a30846f2530363b1450379
ChandrakantPal <sanjay161097@gmail.com>
2021-09-26 01:44:13 +0530

    enum activity

---
+++ activities/src/bin/a7.rs
@@ -0,0 +1,31 @@
+// Topic: Working with an enum
+//
+// Program requirements:
+// * Prints the name of a color to the terminal
+//
+// Notes:
+// * Use an enum with color names as variants
+// * Use a function to print the color name
+// * The function must use the enum as a parameter
+// * Use a match expression to determine which color
+//   name to print
+
+enum Color {
+    Red,
+    Yellow,
+    Blue,
+}
+
+fn print_color(my_color: Color) {
+    match my_color {
+        Color::Red => println!("Red"),
+        Color::Blue => println!("Blue"),
+        Color::Yellow => println!("Yellow"),
+    }
+}
+
+fn main() {
+    print_color(Color::Blue);
+    print_color(Color::Yellow);
+    print_color(Color::Red);
+}
