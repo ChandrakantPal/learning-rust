22bee17f6694aa8522107139d59b50107373e152
ChandrakantPal <sanjay161097@gmail.com>
2021-09-26 00:20:45 +0530

    match expression demo

---
+++ activities/src/bin/matchDemo.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let my_name = "Bob";
+    match my_name {
+        "Sanjay" => println!("thats is my name"),
+        "Bob" => println!("not my name"),
+        "Alice" => println!("hello alice"),
+        _ => println!("nice to meet you!"),
+    }
+}
