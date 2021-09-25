c3278013ac5a8d427e783381f0ba65f3f78a331c
ChandrakantPal <sanjay161097@gmail.com>
2021-09-26 01:35:50 +0530

    enum demo added

---
+++ activities/src/bin/enum-demo.rs
@@ -0,0 +1,11 @@
+enum Direction {
+    Left,
+    Right,
+}
+fn main() {
+    let go = Direction::Left;
+    match go {
+        Direction::Left => println!("go left"),
+        Direction::Right => println!("go right"),
+    }
+}
