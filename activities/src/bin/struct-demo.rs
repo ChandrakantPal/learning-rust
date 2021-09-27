0f5dab4e8888e3dfb42af5eac96cfc2952a42c7d
ChandrakantPal <sanjay161097@gmail.com>
2021-09-28 00:01:13 +0530

    basic struct

--- activities/src/bin/struct-demo.rs
+++ activities/src/bin/struct-demo.rs
@@ -1,2 +1,14 @@
-struct GroceryItem {}
-fn main() {}
+struct GroceryItem {
+    stock: i32,
+    price: f64,
+}
+
+fn main() {
+    let cereal = GroceryItem {
+        stock: 10,
+        price: 2.99,
+    };
+
+    println!("stock: {:?}",cereal.stock)
+    println!("price: {:?}",cereal.price)
+}
