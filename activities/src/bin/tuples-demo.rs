b7dbd0701f297054b09e42336dde24b6db578f94
ChandrakantPal <sanjay161097@gmail.com>
2021-09-30 23:45:10 +0530

    destructuring

--- activities/src/bin/tuples-demo.rs
+++ activities/src/bin/tuples-demo.rs
@@ -1,4 +1,9 @@
 fn main() {
     let coord = (2, 3);
     println!("{:?}", "{:?}", coord.0, coord.1);
+
+    let (x, y) = (2, 3);
+    println!("{:?}", "{:?}", x, y);
+
+    let (name, age) = ("Emma", 20);
 }
