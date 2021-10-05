132fcf20b43687ee23dcec2b1e4ac0109d9f15c3
ChandrakantPal <sanjay161097@gmail.com>
2021-10-05 23:31:44 +0530

    impl used to add function inside a struct

--- activities/src/bin/impl-demo.rs
+++ activities/src/bin/impl-demo.rs
@@ -2,11 +2,13 @@ struct Temprature {
     degrees_f: f64,
 }
 
-fn show_temp(temp: Temprature) {
-    println!("{:} degrees F", temp.degrees_f);
+impl Temprature {
+    fn show_temp(temp: Temprature) {
+        println!("{:} degrees F", temp.degrees_f);
+    }
 }
 
 fn main() {
     let hot = Temprature { degrees_f: 99.9 };
-    show_temp(hot)
+    Temprature::show_temp(hot);
 }
