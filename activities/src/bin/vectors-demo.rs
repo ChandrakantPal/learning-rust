8057e09522df176084366c64162144f2237d7acb
ChandrakantPal <sanjay161097@gmail.com>
2021-10-07 23:49:37 +0530

    displaying elements of a vector

--- activities/src/bin/vectors-demo.rs
+++ activities/src/bin/vectors-demo.rs
@@ -1,1 +1,15 @@
-fn main() {}
+struct Test {
+    score: i32,
+}
+fn main() {
+    let my_scores = vec![
+        Test { score: 90 },
+        Test { score: 80 },
+        Test { score: 70 },
+        Test { score: 60 },
+    ];
+
+    for test in my_scores {
+        println!("score: {:?}", test.score)
+    }
+}
