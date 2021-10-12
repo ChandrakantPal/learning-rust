f4138fd24e5ce1f82fd440a6610b42ee6b98f26a
ChandrakantPal <sanjay161097@gmail.com>
2021-10-12 23:11:09 +0530

    print name fn

--- activities/src/bin/string-demo.rs
+++ activities/src/bin/string-demo.rs
@@ -3,6 +3,10 @@ struct LineItem {
     count: i32,
 }
 
+fn print_name(name: &str) {
+    println!("name {:?}", name);
+}
+
 fn main() {
     let receipt = vec![
         LineItem {
@@ -15,6 +19,7 @@ fn main() {
         },
     ];
     for item in receipt {
-        println!("name {:?}, quantity {:?}", item.name, item.count)
+        print_name(&item.name);
+        println!("quantity {:?}", item.count);
     }
 }
