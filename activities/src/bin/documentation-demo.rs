dca4add34878fe8d5c784244c966daa693066671
ChandrakantPal <sanjay161097@gmail.com>
2021-10-21 23:24:15 +0530

    documentations added

--- activities/src/bin/documentation-demo.rs
+++ activities/src/bin/documentation-demo.rs
@@ -1,12 +1,16 @@
+/// A favorite color.
 enum Color {
     Red,
     Blue,
 }
 
+/// A piece of mail.
 struct Mail {
+    /// The destination address.
     address: String,
 }
 
+/// Adds tow numbers together.
 fn add(a: i32, b: i32) -> i32 {
     a + b
 }
