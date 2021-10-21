c90253162d17984887222ebfed7782d476bf5a1e
ChandrakantPal <sanjay161097@gmail.com>
2021-10-21 23:33:46 +0530

    result data type used

---
+++ activities/src/bin/result-demo.rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+enum MenuChoice {
+    MainMenu,
+    Start,
+    Quit,
+}
+
+fn get_choice(input: &str) -> Result<MenuChoice, String> {
+    match input {
+        "mainmenu" => Ok(MenuChoice::MainMenu),
+        "start" => Ok(MenuChoice::Start),
+        "quit" => Ok(MenuChoice::Quit),
+        _ => Err("menu choice not found".to_owned()),
+    }
+}
+
+fn main() {
+    let choice = get_choice("mainmenu");
+    println!("choice = {:?}", choice);
+}
