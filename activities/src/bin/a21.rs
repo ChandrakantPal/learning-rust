01cf0956f0e494e12089957de8122c277aa8bc61
ChandrakantPal <sanjay161097@gmail.com>
2021-10-27 23:26:10 +0530

    test user not found condition

--- activities/src/bin/a21.rs
+++ activities/src/bin/a21.rs
@@ -26,7 +26,7 @@ fn find_user(name: &str) -> Option<i32> {
 }
 
 fn main() {
-    let user_name = "sam";
+    let user_name = "amy";
     let user = find_user(user_name).map(|user_id| User {
         user_id,
         name: user_name.to_owned(),
