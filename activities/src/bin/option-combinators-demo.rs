f1e96175fea660e4c769ccf613a96d8aa92ca85a
ChandrakantPal <sanjay161097@gmail.com>
2021-10-28 23:19:07 +0530

    or else combinator

--- activities/src/bin/option-combinators-demo.rs
+++ activities/src/bin/option-combinators-demo.rs
@@ -4,4 +4,5 @@ fn main() {
     let a_is_none = a.is_none();
     let a_mapped = a.map(|num| num + 1);
     let a_filtered = a.filter(|num| num == &1); // & since it's browed
+    let a_or_else = a.or_else(|| Some(5));
 }
