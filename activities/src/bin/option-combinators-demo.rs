a9e3207e02d296f81e24643dfecb3fee5698a552
ChandrakantPal <sanjay161097@gmail.com>
2021-10-28 23:37:13 +0530

    unwrapped or else combinator

--- activities/src/bin/option-combinators-demo.rs
+++ activities/src/bin/option-combinators-demo.rs
@@ -5,4 +5,5 @@ fn main() {
     let a_mapped = a.map(|num| num + 1);
     let a_filtered = a.filter(|num| num == &1); // & since it's browed
     let a_or_else = a.or_else(|| Some(5));
+    let unwrapped = a.unwrap_or_else(|| 0);
 }
