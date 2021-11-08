0cdb00dc58decf41a38342e1b4e2a6901744e468
ChandrakantPal <sanjay161097@gmail.com>
2021-11-08 22:49:38 +0530

    find function

--- activities/src/bin/iterator-demo.rs
+++ activities/src/bin/iterator-demo.rs
@@ -9,4 +9,7 @@ fn main() {
     let plus_one: Vec<_> = numbers.iter().map(|num| num + 1).collect();
 
     let new_numbers: Vec<_> = numbers.iter().filter(|num| num <= 3).collect();
+
+    let numbers = vec![1, 2, 3, 4, 5];
+    let find_me: Option<i32> = numbers.iter().find(|num| num == 3);
 }
