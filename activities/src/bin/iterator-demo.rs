f9458fe9d4a2308304fd05f127f560fa81aed6e2
ChandrakantPal <sanjay161097@gmail.com>
2021-11-08 22:47:05 +0530

    filter and map on vector

---
+++ activities/src/bin/iterator-demo.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let numbers = vec![1, 2, 3, 4, 5];
+
+    // let mut plus_one = vec![];
+    // for num in numbers {
+    //     plus_one.push(num + 1);
+    // }
+
+    let plus_one: Vec<_> = numbers.iter().map(|num| num + 1).collect();
+
+    let new_numbers: Vec<_> = numbers.iter().filter(|num| num <= 3).collect();
+}
