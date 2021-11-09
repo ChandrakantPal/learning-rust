d4dfd9d5f4b37b23b951f33da359fbae0253a414
ChandrakantPal <sanjay161097@gmail.com>
2021-11-09 23:25:13 +0530

    printing the number

--- activities/src/bin/a24.rs
+++ activities/src/bin/a24.rs
@@ -12,6 +12,10 @@ fn main() {
     let data: Vec<_> = vec![1, 2, 3, 4, 5]
         .iter()
         .map(|num| num * 3)
-        .filter(|num| num > 10)
+        .filter(|num| num > &10)
         .collect();
+
+    for num in data {
+        println!("{:?}", num);
+    }
 }
