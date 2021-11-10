5a3733c7af466f57ed11a192bbe5d1f80d80619d
ChandrakantPal <sanjay161097@gmail.com>
2021-11-10 23:09:12 +0530

    range used

--- activities/src/bin/range-demo.rs
+++ activities/src/bin/range-demo.rs
@@ -1,1 +1,8 @@
-fn main() {}
+fn main() {
+    let range = 1..=3; // includes the last value
+    let range = 1..4; // doesn't include last value
+
+    for num in 1..4 {
+        println!("{:?}", num);
+    }
+}
