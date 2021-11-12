7041496b51ce65ff5f7198a6d0f5cb933463612a
ChandrakantPal <sanjay161097@gmail.com>
2021-11-12 23:18:21 +0530

    basic use case of if let

--- activities/src/bin/iflet-demo.rs
+++ activities/src/bin/iflet-demo.rs
@@ -1,1 +1,13 @@
-fn main() {}
+fn main() {
+    let maybe_user = Some("Jerry");
+    match maybe_user {
+        Some(user) => println!("user={:?}}", user),
+        None => println!("no user"),
+    }
+
+    if let Some(user) = maybe_user {
+        println!("user={:?}}", user);
+    } else {
+        println!("no user");
+    }
+}
