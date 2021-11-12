71f6f33516eb9f3fad5db0b64d71f61ae33df05d
ChandrakantPal <sanjay161097@gmail.com>
2021-11-12 23:23:26 +0530

    match block removed

--- activities/src/bin/iflet-demo.rs
+++ activities/src/bin/iflet-demo.rs
@@ -6,10 +6,6 @@ enum Color {
 
 fn main() {
     let maybe_user = Some("Jerry");
-    match maybe_user {
-        Some(user) => println!("user={:?}}", user),
-        None => println!("no user"),
-    }
 
     if let Some(user) = maybe_user {
         println!("user={:?}}", user);
