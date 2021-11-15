2781d5337b3e6131bdeed8ad95e8b161c28aa49a
ChandrakantPal <sanjay161097@gmail.com>
2021-11-15 23:52:12 +0530

    math module

--- activities/src/bin/modules-demo.rs
+++ activities/src/bin/modules-demo.rs
@@ -1,4 +1,6 @@
+// use std::collections::HashMap;  available only in main function
 mod greet {
+    // use std::collections::HashMap available only in greet module
     fn hello() {
         println!("hello");
     }
@@ -8,4 +10,18 @@ mod greet {
     }
 }
 
-fn main() {}
+mod math {
+    fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+
+    fn sub(a: i32, b: i32) -> i32 {
+        a - b
+    }
+}
+
+fn main() {
+    use greet::hello;
+    hello();
+    greet::goodbye();
+}
