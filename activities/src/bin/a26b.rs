0417924fae4c53eeb2e6b640f0856fc4344eba73
ChandrakantPal <sanjay161097@gmail.com>
2021-11-16 22:47:56 +0530

    math functions moved to math module

--- activities/src/bin/a26b.rs
+++ activities/src/bin/a26b.rs
@@ -30,16 +30,16 @@ mod msg {
         format!("{}!", msg)
     }
 }
-mod math {}
-
-fn add(lhs: isize, rhs: isize) -> isize {
-    lhs + rhs
-}
-fn sub(lhs: isize, rhs: isize) -> isize {
-    lhs - rhs
-}
-fn mul(lhs: isize, rhs: isize) -> isize {
-    lhs * rhs
+mod math {
+    fn add(lhs: isize, rhs: isize) -> isize {
+        lhs + rhs
+    }
+    fn sub(lhs: isize, rhs: isize) -> isize {
+        lhs - rhs
+    }
+    fn mul(lhs: isize, rhs: isize) -> isize {
+        lhs * rhs
+    }
 }
 
 fn main() {
