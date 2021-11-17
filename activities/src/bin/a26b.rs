daecb885afa96040b69b4ba74582dbe43e4399a8
ChandrakantPal <sanjay161097@gmail.com>
2021-11-17 22:24:43 +0530

    use mod functions and making them public to acess them

--- activities/src/bin/a26b.rs
+++ activities/src/bin/a26b.rs
@@ -16,28 +16,28 @@
 //   `cargo check --bin a26b` to get a listing of required code changes
 
 mod msg {
-    fn trim(msg: &str) -> &str {
+    pub fn trim(msg: &str) -> &str {
         msg.trim()
     }
-    fn capitalize(msg: &str) -> std::borrow::Cow<'_, str> {
+    pub fn capitalize(msg: &str) -> std::borrow::Cow<'_, str> {
         if let Some(letter) = msg.get(0..1) {
             format!("{}{}", letter.to_uppercase(), &msg[1..msg.len()]).into()
         } else {
             msg.into()
         }
     }
-    fn exciting(msg: &str) -> String {
+    pub fn exciting(msg: &str) -> String {
         format!("{}!", msg)
     }
 }
 mod math {
-    fn add(lhs: isize, rhs: isize) -> isize {
+    pub fn add(lhs: isize, rhs: isize) -> isize {
         lhs + rhs
     }
-    fn sub(lhs: isize, rhs: isize) -> isize {
+    pub fn sub(lhs: isize, rhs: isize) -> isize {
         lhs - rhs
     }
-    fn mul(lhs: isize, rhs: isize) -> isize {
+    pub fn mul(lhs: isize, rhs: isize) -> isize {
         lhs * rhs
     }
 }
@@ -45,28 +45,30 @@ mod math {
 fn main() {
     // Part 1: math functions
     let result = {
-        let two_plus_two = add(2, 2);
-        let three = sub(two_plus_two, 1);
-        mul(three, three)
+        let two_plus_two = math::add(2, 2);
+        let three = math::sub(two_plus_two, 1);
+        math::mul(three, three)
     };
 
     // Ensure we have a correct result.
     assert_eq!(result, 9);
     println!("(2 + 2 - 1) * 3 = {}", result);
 
-    // Part 2: string functions
-    let hello = {
-        let msg = "hello ";
-        let msg = trim(msg);
-        capitalize(msg)
-    };
-    let world = {
-        let msg = "world";
-        exciting(msg)
-    };
-    let msg = format!("{}, {}", hello, world);
-
-    // Ensure we have a correct result.
-    assert_eq!(&msg, "Hello, world!");
-    println!("{}", msg);
+    {
+        use msg::{capitalize, exciting, trim};
+        // Part 2: string functions
+        let hello = {
+            let msg = "hello ";
+            let msg = trim(msg);
+            capitalize(msg)
+        };
+        let world = {
+            let msg = "world";
+            exciting(msg)
+        };
+        let msg = format!("{}, {}", hello, world);
+        // Ensure we have a correct result.
+        assert_eq!(&msg, "Hello, world!");
+        println!("{}", msg);
+    }
 }
