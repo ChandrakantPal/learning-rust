f9bf6cac626db39fb0007fafea1c299326b9e39d
ChandrakantPal <sanjay161097@gmail.com>
2021-11-23 23:56:23 +0530

    use chrono crate

---
+++ activities/src/bin/a26.rs
@@ -0,0 +1,12 @@
+// Topic: External crates
+//
+// Requirements:
+// * Display the current date and time
+//
+// Notes:
+// * Use the `chrono` crate to work with time
+// * (OPTIONAL) Read the documentation section `Formatting and Parsing`
+//   for examples on how to create custom time formats
+use chrono::prelude::*;
+
+fn main() {}
