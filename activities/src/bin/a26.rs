5d30f48e03f58ba6b75ca825699cec91b0ec7bf0
ChandrakantPal <sanjay161097@gmail.com>
2021-11-24 00:02:21 +0530

    custom formating

--- activities/src/bin/a26.rs
+++ activities/src/bin/a26.rs
@@ -11,5 +11,5 @@ use chrono::prelude::*;
 
 fn main() {
     let local: DateTime<Local> = Local::now();
-    println!("{:?}", local);
+    println!("{}", local.format("%Y-%m-%d %H:%M:%S").to_string());
 }
