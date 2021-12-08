13a5c84595db1ee5485049db84046fe4bb8d70bc
ChandrakantPal <sanjay161097@gmail.com>
2021-12-08 22:34:37 +0530

    get input fn created

--- activities/src/bin/user-input-demo.rs
+++ activities/src/bin/user-input-demo.rs
@@ -1,1 +1,9 @@
+use std::io;
+
+fn get_input() -> io::Result<String> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    Ok(buffer.trim().to_owned());
+}
+
 fn main() {}
