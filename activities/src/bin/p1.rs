4d568efd91275cb5609b76918875b07fc5491b64
ChandrakantPal <sanjay161097@gmail.com>
2021-12-13 21:33:41 +0530

    get input function

---
+++ activities/src/bin/p1.rs
@@ -0,0 +1,46 @@
+// Project 1: Interactive bill manager
+//
+// Summary:
+//   Create a command line bills/expenses manager that runs
+//   interactively. This mini project brings together many of
+//   the concepts learn thus far into a single application.
+//
+//   The user stories/requirements are split into stages.
+//   Fully implement each stage as a complete working program
+//   before making changes for the next stage. Leverage the
+//   compiler by using `cargo check --bin p1` when changing
+//   between stages to help identify adjustments that need
+//   to be made.
+//
+// User stories:
+// * Stage 1:
+//   - I want to add bills, including the name and amount owed.
+//   - I want to view existing bills.
+// * Stage 2:
+//   - I want to remove bills.
+// * Stage 3:
+//   - I want to edit existing bills.
+//   - I want to go back if I change my mind.
+//
+// Tips:
+// * Use the loop keyword to create an interactive menu.
+// * Each menu choice should be it's own function, so you can work on the
+//   the functionality for that menu in isolation.
+// * A vector is the easiest way to store the bills at stage 1, but a
+//   hashmap will be easier to work with at stages 2 and 3.
+use std::io;
+
+fn get_input() -> Option<String> {
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer).is_err() {
+        println!("Please enter your data again");
+    }
+    let input = buffer.trim().to_owned();
+    if &input = "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+
+fn main() {}
