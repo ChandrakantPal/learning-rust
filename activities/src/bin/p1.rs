f2539380f1f7039209c1bc5a9d231f1d2d46411e
ChandrakantPal <sanjay161097@gmail.com>
2021-12-16 23:40:59 +0530

    using menu functions in main function

--- activities/src/bin/p1.rs
+++ activities/src/bin/p1.rs
@@ -134,14 +134,15 @@ impl MainMenu {
 
 fn main() {
     // Create bill structure
+    let mut bills = Bills::new();
 
     loop {
         // Display the menu
         MainMenu::show();
         let input = get_input().expect("no data entered");
         match MainMenu::from_str(input.as_str()) {
-            Some(MainMenu::AddBill) => (),
-            Some(MainMenu::ViewBill) => (),
+            Some(MainMenu::AddBill) => menu::add_bill(&mut bills),
+            Some(MainMenu::ViewBill) => menu::view_bills(&bills),
             None => return,
         }
         // Make a choice, based on input
