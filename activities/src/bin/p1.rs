c66bf19d9cf60c014767457d1d396bbe7dd2350d
ChandrakantPal <sanjay161097@gmail.com>
2021-12-17 23:47:23 +0530

    menu mod remove bill function

--- activities/src/bin/p1.rs
+++ activities/src/bin/p1.rs
@@ -109,6 +109,24 @@ mod menu {
         println!("Bill added");
     }
 
+    pub fn remove_bill(bills: &mut Bills) {
+        for bill in bills.get_all() {
+            println!("{:?}", bill);
+        }
+        println!("Enter bill name to remove");
+
+        let name = match get_input() {
+            Some(name) => name,
+            None => return,
+        };
+
+        if bills.remove(&name) {
+            println!("bill removed");
+        } else {
+            println!("bill not found");
+        }
+    }
+
     pub fn view_bills(bills: &Bills) {
         for bill in bills.get_all() {
             println!("{:?}", bill);
