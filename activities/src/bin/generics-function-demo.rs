61b7985c628d9ccf959c2084a92fb857392195a9
ChandrakantPal <sanjay161097@gmail.com>
2022-01-10 23:49:48 +0530

    struct for Passenger and checkin trait

--- activities/src/bin/generics-function-demo.rs
+++ activities/src/bin/generics-function-demo.rs
@@ -13,4 +13,14 @@ impl CheckIn for Pilot {
     }
 }
 
+struct Passenger;
+impl CheckIn for Passenger {
+    fn check_in(&self) {
+        println!("checked in as passenger");
+    }
+    fn process(&self) {
+        println!("passenger takes a seat");
+    }
+}
+
 fn main() {}
