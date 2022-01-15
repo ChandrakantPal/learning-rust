bc3778c35da5f1ded29416b975ade48db6710cef
ChandrakantPal <sanjay161097@gmail.com>
2022-01-15 23:59:48 +0530

    material trait

---
+++ activities/src/bin/a31.rs
@@ -0,0 +1,27 @@
+// Topic: Trait Objects
+//
+// Summary:
+//   A contractor wants a program that can sum the cost of materials based
+//   on how many square meters are required for a job.
+//
+// Requirements:
+// * Calculate multiple material types with different costs
+// * Must be able to process a list of varying materials
+// * Material types and cost includes:
+//   * Carpet - $10 per square meter
+//   * Tile - $15 per square meter
+//   * Wood - $20 per square meter
+// * Square meters must be taken into account
+//
+// Notes:
+// * Create a trait that can be used to retrieve the cost of a material
+// * Create trait objects and store them in a vector for processing
+// * Use a function to calculate the total cost
+// * Process at least 3 different materials
+
+trait Material {
+    fn cost_per_sq_meter(&self) -> f64;
+    fn square_meters(&self) -> f64;
+}
+
+fn main() {}
