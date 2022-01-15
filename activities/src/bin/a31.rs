20ef6e8c4c42c61c16fbc08b430eac7ecc50fc7a
ChandrakantPal <sanjay161097@gmail.com>
2022-01-16 00:15:32 +0530

    creating material and finding total cost

--- activities/src/bin/a31.rs
+++ activities/src/bin/a31.rs
@@ -59,4 +59,13 @@ fn total_cost(material: &Vec<Box<dyn Material>>) -> f64 {
     material.iter().map(|mat| mat.total_cost()).sum()
 }
 
-fn main() {}
+fn main() {
+    let carpet = Box::new(Carpet(20.0));
+    let tile = Box::new(Tile(10.0));
+    let wood = Box::new(Wood(30.0));
+
+    let materials: Vec<Box<dyn Material>> = vec![carpet, tile, wood];
+
+    let total = total_cost(&materials);
+    println!("cost = ${}", total);
+}
