56503b2b1a64a085d97bbc75e341ef12a2fed3e3
ChandrakantPal <sanjay161097@gmail.com>
2022-01-19 23:00:28 +0530

    longest function

---
+++ activities/src/bin/a33.rs
@@ -0,0 +1,23 @@
+// Topic: Lifetimes & Functions
+//
+// Summary:
+// Create a program that compares which string is longer (highest length).
+//
+// Requirements:
+// * The comparison must be done using a function named `longest`
+// * No data may be copied (cannot use .to_owned() or .to_string())
+// * If both strings are the same length, the first one should be returned
+
+fn longest(one: &str, two: &str) -> &str {
+    if two > one {
+        two
+    } else {
+        one
+    }
+}
+
+fn main() {
+    let short = "hello";
+    let long = "this is a long message";
+    println!("{}", longest(short, long))
+}
