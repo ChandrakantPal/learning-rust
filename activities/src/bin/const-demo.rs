1e765a6e300e9077a8b0fd28e8a52c6e6d7cbabd
ChandrakantPal <sanjay161097@gmail.com>
2022-01-21 22:26:48 +0530

    MAX_SPEED used

--- activities/src/bin/const-demo.rs
+++ activities/src/bin/const-demo.rs
@@ -1,8 +1,8 @@
 const MAX_SPEED: i32 = 9000;
 
 fn clamp_speed(speed: i32) -> i32 {
-    if speed > 9000 {
-        9000
+    if speed > MAX_SPEED {
+        MAX_SPEED
     } else {
         speed
     }
