430356775050b931f46426cf58dfd7eacaecc5e8
ChandrakantPal <sanjay161097@gmail.com>
2022-01-22 22:11:27 +0530

    neverzero impl logic

--- activities/src/bin/new-types-demo.rs
+++ activities/src/bin/new-types-demo.rs
@@ -2,7 +2,13 @@
 struct NeverZero(i32);
 
 impl NeverZero {
-    fn new(i: i32) -> Result<Self, String> {}
+    fn new(i: i32) -> Result<Self, String> {
+        if i == 0 {
+            Err("cannot be zero".to_owned())
+        } else {
+            Ok(Self(i))
+        }
+    }
 }
 
 fn divide(a: i32, b: NeverZero) -> i32 {}
