5d97c59306f9ebfa77579f367bc62776ee4c7842
ChandrakantPal <sanjay161097@gmail.com>
2022-01-26 15:09:31 +0530

    match stage

--- activities/src/bin/match-guards-binding-demo3.rs
+++ activities/src/bin/match-guards-binding-demo3.rs
@@ -8,4 +8,10 @@ enum Difficulty {
 fn main() {
     let stage = 5;
     let diff Difficulty::Normal
+    match stage {
+        s if (s == 5 && diff == Difficulty::Easy) => println!("easy mode stage 5"),
+        s if diff == Difficulty::Normal => println!("normal difficulty stage {}",s),
+        s @ 10 | s @ 15 => println!("stage 10 or 15"),
+        s  => println!("stage {}",stage),
+    }
 }
