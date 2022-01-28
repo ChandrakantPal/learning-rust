8fbbdf933a8a087a82509f350fb95a0fa14d8cbb
ChandrakantPal <sanjay161097@gmail.com>
2022-01-28 15:32:09 +0530

    process_chunk

--- activities/src/bin/a36.rs
+++ activities/src/bin/a36.rs
@@ -14,6 +14,14 @@ fn data() -> &'static [u64] {
     &[5, 5, 4, 4, 3, 3, 1]
 }
 
+fn process_chunk(data: &[u64]) {
+    match data {
+        [lhs, rhs] => println!("{}+{}={}", lhs, rhs, (lhs + rhs)),
+        [single] => println!("Unpaired value: {}", single),
+        [] => println!("Data stream complete"),
+    }
+}
+
 fn main() {
     // `stream` is an iterator of Option<&[u64]>
     let mut stream = data().chunks(2);
