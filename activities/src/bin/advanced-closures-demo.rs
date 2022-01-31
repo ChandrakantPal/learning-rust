952e540f44eff410f8cd8ddf2f93e3acb1427a1b
ChandrakantPal <sanjay161097@gmail.com>
2022-01-31 14:59:12 +0530

    data from outside closure

--- activities/src/bin/advanced-closures-demo.rs
+++ activities/src/bin/advanced-closures-demo.rs
@@ -2,8 +2,9 @@ fn math(a: i32, b: i32, op: Box<dyn Fn(i32, i32) -> i32>) -> i32 {
     op(a, b)
 }
 fn main() {
+    let name = "Sanjay";
     let add = Box::new(|a, b| {
-        println!("adding!");
+        println!("adding a number for {}", name);
         a + b
     });
     let sub = Box::new(|a, b| a - b);
