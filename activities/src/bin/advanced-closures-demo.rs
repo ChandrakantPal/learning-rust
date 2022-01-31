4e9658295cc233cc4a8559dd8fe70bffd43fc31b
ChandrakantPal <sanjay161097@gmail.com>
2022-01-31 14:48:24 +0530

    add closure

--- activities/src/bin/advanced-closures-demo.rs
+++ activities/src/bin/advanced-closures-demo.rs
@@ -1,4 +1,8 @@
 fn math(a: i32, b: i32, op: Box<dyn Fn(i32, i32) -> i32>) -> i32 {
     op(a, b)
 }
-fn main() {}
+fn main() {
+    let add = |a, b| a + b;
+    let add = Box::new(add);
+    println!("{}", math(25, 45, add))
+}
