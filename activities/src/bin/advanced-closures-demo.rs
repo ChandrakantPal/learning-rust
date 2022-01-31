eb24aaed752026f143f7075a748b1fbb63ccfdd7
ChandrakantPal <sanjay161097@gmail.com>
2022-01-31 14:56:59 +0530

    printing adding

--- activities/src/bin/advanced-closures-demo.rs
+++ activities/src/bin/advanced-closures-demo.rs
@@ -2,8 +2,11 @@ fn math(a: i32, b: i32, op: Box<dyn Fn(i32, i32) -> i32>) -> i32 {
     op(a, b)
 }
 fn main() {
-    let add = Box::new(|a, b| a + b);
+    let add = Box::new(|a, b| {
+        println!("adding!");
+        a + b
+    });
     let sub = Box::new(|a, b| a - b);
     let mul = Box::new(|a, b| a * b);
-    println!("{}", math(25, 45, add))
+    println!("{}", math(25, 45, add));
 }
