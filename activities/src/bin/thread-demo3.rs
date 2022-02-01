bd24b536fb292b70132e47123a15df84e37ea079
ChandrakantPal <sanjay161097@gmail.com>
2022-02-01 14:07:06 +0530

    caps thread

---
+++ activities/src/bin/thread-demo3.rs
@@ -0,0 +1,8 @@
+use std::thread;
+
+fn main() {
+    let data = vec!['a', 'b', 'c'];
+    let caps = thread::spawn(move || {
+        let data: Vec<char> = data.iter().map(|c| c.to_ascii_uppercase()).collect();
+    });
+}
