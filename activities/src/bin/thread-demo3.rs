4d88f10ecb19c3c51274dbfb3ba7f17020b7a325
ChandrakantPal <sanjay161097@gmail.com>
2022-02-01 14:07:45 +0530

    returning the data

--- activities/src/bin/thread-demo3.rs
+++ activities/src/bin/thread-demo3.rs
@@ -4,5 +4,6 @@ fn main() {
     let data = vec!['a', 'b', 'c'];
     let caps = thread::spawn(move || {
         let data: Vec<char> = data.iter().map(|c| c.to_ascii_uppercase()).collect();
+        data
     });
 }
