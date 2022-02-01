042fd07a84d268fe3902367e6de1a959a71462a4
ChandrakantPal <sanjay161097@gmail.com>
2022-02-01 13:53:37 +0530

    value thread

--- activities/src/bin/thread-demo2.rs
+++ activities/src/bin/thread-demo2.rs
@@ -1,2 +1,9 @@
 use std::thread;
 use std::time::Duration;
+
+fn main() {
+    let value: JoinHandle<usize> = thread::spawn(move || {
+        thread::sleep(Duration::from_secs(1));
+        42
+    });
+}
