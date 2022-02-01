5a2c3987f1a974501b6f5427d08abdfdc36fb56e
ChandrakantPal <sanjay161097@gmail.com>
2022-02-01 13:44:22 +0530

    thread a

--- activities/src/bin/thread-demo1.rs
+++ activities/src/bin/thread-demo1.rs
@@ -1,1 +1,10 @@
 use std::thread;
+
+fn main() {
+    let iterations = 10;
+    let a = thread::spawn(move || {
+        for i in 1..=iterations {
+            println!("A:{}", i);
+        }
+    });
+}
