01811a66f7c364f84fa11248076289a488bfeaf6
ChandrakantPal <sanjay161097@gmail.com>
2022-02-03 15:05:59 +0530

    displaying disconnected

--- activities/src/bin/channels-demo.rs
+++ activities/src/bin/channels-demo.rs
@@ -29,6 +29,7 @@ fn main() {
 
     s.send(ThreadMsg::PrintData("hello from main".to_owned()));
     s.send(ThreadMsg::Sum(10, 10));
-    s.send(ThreadMsg::Quit);
+    // s.send(ThreadMsg::Quit);
+    drop(s);
     handle.join();
 }
