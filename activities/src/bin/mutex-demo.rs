1cc68ea6782db74f23c16d5ad175a9a63dca2b73
ChandrakantPal <sanjay161097@gmail.com>
2022-02-08 15:44:36 +0530

    display string not method

--- activities/src/bin/mutex-demo.rs
+++ activities/src/bin/mutex-demo.rs
@@ -11,7 +11,7 @@ struct DigitalSignBoard {
 
 impl DigitalSignBoard {
     fn update(&self) {
-        let data = self.display().lock();
+        let data = self.display.lock();
         println!("sign data='{}'", data);
         // place on digital sign
     }
@@ -33,6 +33,7 @@ fn spawn_display_thread(display_data: SharedSignData) {
 fn change_data(display_data: SharedSignData, new_data: &str) {
     let mut data = display_data.lock();
     *data = new_data.to_owned();
+    println!("-----updated:{}", new_data);
 }
 
 fn main() {
